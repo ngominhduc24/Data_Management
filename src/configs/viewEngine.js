2e777510ad2927aa6420aeeb1984c02529d5bdde
duc minh <ngominhduc100@gmail.com>
2023-05-04 16:12:36 +0700

    .

--- src/configs/viewEngine.js
+++ src/configs/viewEngine.js
@@ -1,7 +1,8 @@
 import express from "express";
 
 const configviewEngine = (app) => {
-  app.use(express.static("./src/public"));
+  // app.use(express.static("./src/public"));
+  app.use(express.static(__dirname + "/public"));
   app.set("views", "./src/views");
 };
 
