6b6d41c4d5dfa9cbe404c3363d754dcfb7d13566
duc minh <ngominhduc100@gmail.com>
2023-05-04 22:11:06 +0700

    add static file

--- src/configs/viewEngine.js
+++ src/configs/viewEngine.js
@@ -1,7 +1,8 @@
 import express from "express";
-
+import path from "path";
 const configviewEngine = (app) => {
-  app.use(express.static("./src/public"));
+  // app.use(express.static("./src/public"));
+  app.use(express.static(path.join(__dirname, "../public")));
   app.set("views", "./src/views");
 };
 
